99524bd3a052de8b8ae9cfe97169abd523d73804
“JapaKaneto” <pedro_kaneto@hotmail.com>
2023-06-01 16:20:12 -0300

    alt avartar js

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ function toggleMode() {
 
   const img = document.querySelector("#profile img")
 
-  if(html.classList.contains("light")) {
+  if (html.classList.contains("light")) {
     img.setAttribute("src", "./assets/avatar-light.png")
     img.setAttribute("alt", "Pedro sorrindo com o fundo azul claro")
   } else {
-    img.setAttribute("src", "/assets/avatar-dark.png")
+    img.setAttribute("src", "./assets/avatar-dark.png")
     img.setAttribute("alt", "Pedro sorrindo com o fundo azul escuro")
   }
 }
